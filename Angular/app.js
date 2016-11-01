873f0db10ea990c49d37895f0477c280b6a203d5
Chris Jaffier <chrisd@Chriss-MacBook-Pro.local>
2016-11-01 18:06:14 -0400

    categories and disasters added

--- Angular/app.js
+++ Angular/app.js
@@ -1,27 +1,43 @@
-"use strict";
-
-(function(){
-  angular
-  .module("disaster", [
+angular
+  .module("nda", [
     "ui.router",
-    "disasters"
+    "ngResource"
   ])
   .config([
     "$stateProvider",
     RouterFunction
+
+.controller("CategoryIndexController", [
+  "CategoryFactory",
+  CategoryIndexControllerFunction
+])
+
+.controller("DisasterIndexController", [
+  "DisasterFactory",
+  DisasterIndexControllerFunction
+])
   ]);
 
   function RouterFunction($stateProvider){
     $stateProvider
+    .state("categoryIndex", {
+      templateUrl: "js/categories/index.html",
+      controller: "CategoriesIndexController",
+      controllerAs: "vm"
+    })
     .state("disasterIndex", {
       url: "/disasters",
       templateUrl: "js/disasters/index.html",
       controller: "DisasterIndexController",
-      controllerAs: "DisasterIndexViewModel"
+      controllerAs: "vm"
     })
     .state("disasterShow", {
       url: "/disasters/:id",
       templateUrl: "js/disasters/show.html"
+      controller: "DisasterShowController",
+      controllerAs: "vm"
     });
   }
-}());
+  function FactoryFunction($resource){
+    return $resource("http://localhost:3000/categories/:id");
+  }  ());
