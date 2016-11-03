90f6ce91166f434845a9b8f430167b8e75d8ae4e
manish pradhan <pradhanmanish33@gmail.com>
2016-11-03 02:39:29 -0400

    commit

--- Angular/js/app.js
+++ Angular/js/app.js
@@ -1,85 +1,86 @@
+angular
+ .module("nda", [
+   "ui.router",
+   "ngResource",
+ ])
+ .config([
+   "$stateProvider",
+   RouterFunction
+ ])
+ .factory("CategoryFactory", [
+   "$resource",
+   CategoryFactoryFunction
+ ])
+ .factory("DisasterFactory", [
+   "$resource",
+   DisasterFactoryFunction
+ ])
 
-  angular
-  .module("nda", [
-    "ui.router",
-    "ngResource",
-  ])
-  .config([
-    "$stateProvider",
-    RouterFunction
-  ])
-  .factory("CategoryFactory", [
-    "$resource",
-    CategoryFactoryFunction
-  ])
-  .factory("DisasterFactory", [
-    "$resource",
-    DisasterFactoryFunction
-  ])
+ .controller("CategoryIndexController", [
+   "CategoryFactory",
+   CategoryIndexControllerFunction
+ ])
+ .controller("DisasterIndexController", [
+   "DisasterFactory",
+   DisasterIndexControllerFunction
+ ])
+ .controller("DisasterShowController", [
+   "$stateParams",
+   "DisasterFactory",
+   DisasterShowControllerFunction
 
-  .controller("CategoryIndexController", [
-    "CategoryFactory",
-    CategoryIndexControllerFunction
-  ])
-  .controller("DisasterIndexController", [
-    "DisasterFactory",
-    DisasterIndexControllerFunction
-  ])
-  .controller("DisasterShowController", [
-    "DisasterFactory",
-    DisasterShowControllerFunction
-
-  ])
-  .controller("CategoryShowController", [
-    "$stateParams",
-    "CategoryFactory",
-    CategoryShowControllerFunction
-  ])
+ ])
+ .controller("CategoryShowController", [
+   "$stateParams",
+   "CategoryFactory",
+   CategoryShowControllerFunction
+ ])
 
 function CategoryFactoryFunction($resource){
-  return $resource("http://localhost:3000/categories/:id.json");
+ return $resource("http://localhost:3000/categories/:id.json");
 }
 function CategoryIndexControllerFunction(CategoryFactory) {
-  this.categories = CategoryFactory.query();
+ this.categories = CategoryFactory.query();
 }
 function CategoryShowControllerFunction($stateParams, CategoryFactory){
-  this.category = CategoryFactory.get({id: $stateParams.id});
+ this.category = CategoryFactory.get({id: $stateParams.id});
 }
 function DisasterFactoryFunction($resource){
-  return $resource("http://localhost:3000/disasters/:id.json")
+ return $resource("http://localhost:3000/disasters/:id.json")
 }
 function DisasterIndexControllerFunction(DisasterFactory){
-  this.disaster = DisasterFactory.query();
+ this.disaster = DisasterFactory.query();
 }
 
 function DisasterShowControllerFunction($stateParams, DisasterFactory){
-  this.disaster = DisasterFactory.get({id: $stateParams.id});
+
+ this.disaster = DisasterFactory.get({id: $stateParams.id});
 }
 
-  function RouterFunction($stateProvider){
-    $stateProvider
-    .state("categoryIndex", {
-      url: "/categories",
-      templateUrl: "js/ng-views/categories/index.html",
-      controller: "CategoryIndexController",
-      controllerAs: "vm"
-    })
-    .state("categoryShow", {
-      url: "/categories/:id",
-      templateUrl: "js/ng-views/categories/show.html",
-      controller: "CategoryShowController",
-      controllerAs: "vm"
-    })
-    .state("disasterIndex", {
-      url: "/disasters",
-      templateUrl: "js/ng-views/disasters/index.html",
-      controller: "DisasterIndexController",
-      controllerAs: "vm"
-    })
-    .state("disasterShow", {
-      url: "/disasters/:id",
-      templateUrl: "js/ng-views/disasters/show.html",
-      controller: "DisasterShowController",
-      controllerAs: "vm"
-    });
-  }
+ function RouterFunction($stateProvider){
+   $stateProvider
+   .state("categoryIndex", {
+     url: "/categories",
+     templateUrl: "js/ng-views/categories/index.html",
+     controller: "CategoryIndexController",
+     controllerAs: "vm"
+   })
+   .state("categoryShow", {
+     url: "/categories/:id",
+     templateUrl: "js/ng-views/categories/show.html",
+     controller: "CategoryShowController",
+     controllerAs: "vm"
+   })
+   .state("disasterIndex", {
+     url: "/disasters",
+     templateUrl: "js/ng-views/disasters/index.html",
+     controller: "DisasterIndexController",
+     controllerAs: "vm"
+   })
+   .state("disasterShow", {
+     url: "/disasters/:id",
+     templateUrl: "js/ng-views/disasters/show.html",
+     controller: "DisasterShowController",
+     controllerAs: "vm"
+   });
+ }
