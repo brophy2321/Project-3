70ff2077588b9a9e5128ff53abcbea8929baeb3f
RYANBROPHY <BROPHY2321@ME.COM>
2016-11-03 11:25:22 -0400

    angular comments

--- Angular/js/app.js
+++ Angular/js/app.js
@@ -10,50 +10,52 @@ angular
 ])
 .factory("CategoryFactory", [
   "$resource",
-    CategoryFactoryFunction
-  ])
-  .factory("DisasterFactory", [
-    "$resource",
-    DisasterFactoryFunction
-  ])
-  .factory("CommentFactory", [
-    "$resource",
-    CommentFactoryFunction
-  ])
+  CategoryFactoryFunction
+])
+.factory("DisasterFactory", [
+  "$resource",
+  DisasterFactoryFunction
+])
+.factory("CommentFactory", [
+  "$resource",
+  CommentFactoryFunction
+])
 
-  .controller("CategoryIndexController", [
-    "CategoryFactory",
-    CategoryIndexControllerFunction
-  ])
-  .controller("CategoryShowController", [
-    "$stateParams",
-    "CategoryFactory",
-    CategoryShowControllerFunction
-  ])
-  .controller("DisasterIndexController", [
-    "DisasterFactory",
-    DisasterIndexControllerFunction
-  ])
-  .controller("DisasterShowController", [
-    "$stateParams",
-    "DisasterFactory",
-    DisasterShowControllerFunction
+.controller("CategoryIndexController", [
+  "CategoryFactory",
+  CategoryIndexControllerFunction
+])
+.controller("CategoryShowController", [
+  "$stateParams",
+  "CategoryFactory",
+  CategoryShowControllerFunction
+])
+.controller("DisasterIndexController", [
+  "DisasterFactory",
+  DisasterIndexControllerFunction
+])
+.controller("DisasterShowController", [
+  "$stateParams",
+  "DisasterFactory",
+  "CommentFactory",
+  DisasterShowControllerFunction
 
-  ])
 
-  .controller("CommentIndexController", [
-    "CommentFactory",
-    CommentIndexControllerFunction
-  ])
-  .controller("CommentShowController", [
-    "$stateParams",
-    "CommentFactory",
-    CommentShowControllerFunction
-  ])
-  .controller("CommentNewController", [
-    "CommentFactory",
-    CommentNewControllerFunction
-  ])
+])
+
+.controller("CommentIndexController", [
+  "CommentFactory",
+  CommentIndexControllerFunction
+])
+.controller("CommentShowController", [
+  "$stateParams",
+  "CommentFactory",
+  CommentShowControllerFunction
+])
+.controller("CommentNewController", [
+  "CommentFactory",
+  CommentNewControllerFunction
+])
 
 
 
@@ -74,29 +76,36 @@ function DisasterIndexControllerFunction(DisasterFactory){
   this.disaster = DisasterFactory.query();
 }
 
-function DisasterShowControllerFunction($stateParams, DisasterFactory){
+function DisasterShowControllerFunction($stateParams, DisasterFactory, CommentFactory){
+  var self = this
+  this.disaster = DisasterFactory.get({id: $stateParams.id}, function(){
+    self.comments = self.disaster.comments
 
-  this.disaster = DisasterFactory.get({id: $stateParams.id});
-}
-
-function CommentFactoryFunction($resource){
-  return $resource("http://localhost:3000/disasters/:id.json", {},{
-        update: {method: "PUT"}
   });
-}
-function CommentIndexControllerFunction(CommentFactory){
-  this.comment = CommentFactory.query();
-}
-function CommentNewControllerFunction( CommentFactory ){
-      this.comment = new CommentFactory();
-      this.create = function(){
-        this.comment.$save()
-      }
-    }
-function CommentShowControllerFunction($stateParams, CommentFactory){
+  this.comment = new CommentFactory();
+  this.create = function(){
+    this.comment.$save({disaster_id: $stateParams.id})
 
-  this.comment = CommentFactory.get({id: $stateParams.id});
+  }
 }
+  function CommentFactoryFunction($resource){
+    return $resource("http://localhost:3000/disasters/:disaster_id/comments/:id.json", {},{
+      update: {method: "PUT"}
+    });
+  }
+  function CommentIndexControllerFunction(CommentFactory){
+    this.comment = CommentFactory.query();
+  }
+  function CommentNewControllerFunction( CommentFactory ){
+    this.comment = new CommentFactory();
+    this.create = function(){
+      this.comment.$save()
+    }
+  }
+  function CommentShowControllerFunction($stateParams, CommentFactory){
+
+    this.comment = CommentFactory.get({id: $stateParams.id});
+  }
 
   function RouterFunction($stateProvider){
     $stateProvider
@@ -125,20 +134,20 @@ function CommentShowControllerFunction($stateParams, CommentFactory){
       controllerAs: "vm"
     })
     .state("commentIndex", {
-      url:"/comments/:id",
-      templateUrl:"js/ng-views/comments/index.html",
+      url:"/disaster/comments/:id",
+      templateUrl:"js/ng-views/disasters/comments/index.html",
       controller:"CommentIndexController",
       controllerAs:"vm"
     })
     .state("commentNew", {
-      url:"/comments/new",
-      templateUrl:"js/ng-views/new.html",
+      url:"/disaster/comments/new",
+      templateUrl:"js/ng-views/disasters/comment/new.html",
       controller:"CommentNewController",
       controllerAs:"vm"
     })
     .state("commentShow", {
-      url:"/comments/:id",
-      templateUrl:"js/ng-views/comments/show.html",
+      url:"/disaster/comments/:id",
+      templateUrl:"js/ng-views/disasters/comments/show.html",
       controller:"CommentShowController",
       controllerAs:"vm"
     })
